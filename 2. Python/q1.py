633c446d8acbb7825305b892652867bb9733497b
Arindal Char <110285827+arindal1@users.noreply.github.com>
2022-08-11 20:25:43 +0530

    Create q1.py

---
+++ 2. Python/q1.py
@@ -0,0 +1,10 @@
+# 01. Write a Python program to create an array of 5 integers and display the array items. Access individual elements through indexes.
+
+
+from array import *
+arr = array('i', [1,3,5,7,9,11,13,15,17])
+print(arr)
+print("Access first three items individually: ")
+print(arr[0])
+print(arr[5])
+print(arr[7])
